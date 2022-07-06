eeb1f9b9d972fba65e4463b2c4d912f8fc83dfa2
Surajkumarrr <surajsahu13sk@gmail.com>
2022-07-06 13:51:49 +0530

    fetched data from API and rendered it

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,12 +5,11 @@ export class NewsItem extends Component {
     let {title,description,imgUrl,newsUrl} = this.props ;
     return (
       <div>
-        <h3>news item</h3>
         <div className="card" style={{width: "18rem"}}>
-        <img src={imgUrl} className="card-img-top" alt="..." />
+        <img src={!imgUrl?"https://images.unsplash.com/photo-1482160549825-59d1b23cb208?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fG5ld3NwYXBlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60":imgUrl} className="card-img-top" alt="..." />
         <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{!description?"Click to Read More":description}</p>
             <a href={newsUrl} target="_blank" className="btn btn-primary btn-sm">Read More</a>
         </div>
         </div>
